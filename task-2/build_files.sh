7b137ee1bd522035af4d713541bedfa0bf534991
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2024-07-05 21:25:43 +0100

    Updated python version

--- task-2/build_files.sh
+++ task-2/build_files.sh
@@ -1,3 +1,3 @@
 pip install -r requirements.txt
-python3.12 manage.py collectstatic --no-input --clear
-python3.12 manage.py migrate
+python3 manage.py collectstatic --no-input --clear
+python3 manage.py migrate
